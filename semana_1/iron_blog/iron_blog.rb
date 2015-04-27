26ad1f6afcedddd936cf0fd03ab2325e39aa9fff
ivancarrascal <i.carrascalyuste@gmail.com>
2015-04-27 13:22:52 +0200

    Añadida clase blog a iron-blog

--- semana_1/iron_blog/iron_blog.rb
+++ semana_1/iron_blog/iron_blog.rb
@@ -6,3 +6,22 @@ def initialize title, date, text
 @text = text
 end
 end
+
+class Blog
+	attr_accessor :posts
+	def initialize 
+		@posts = []
+	end
+end
+
+blog = Blog.new
+
+post1 = Post.new( "Güebah", Time.now, "Es lo nuevo este año, en el pasado fue Zasca" )
+post2 = Post.new( "Hello there, I'm using Ruby!", (Time.now) -1000, "Entroducing..." )
+post3 = Post.new( "See you later", Time.now, "Alligator" )
+
+blog.add_post post1
+blog.add_post post2
+blog.add_post post3
+
+blog.show_front_page
